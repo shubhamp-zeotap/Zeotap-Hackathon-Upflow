4ad9e7bc90ecc7018aaad82c421c8cd7b764c392
AnandKumarSP-Zeotap <anand.sp@zeotap.com>
2018-04-07 14:11:23 +0530

    Handled CORS

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -9,15 +9,25 @@ type App struct {
 	*revel.Controller
 }
 
+func prepareHeaders(a *App) {
+	a.Response.Out.Header().Set("Access-Control-Allow-Origin", "*")
+	a.Response.Out.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	a.Response.Out.Header().Set("Access-Control-Allow-Headers",
+		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func (c App) Index() revel.Result {
-	return c.Render()
+	prepareHeaders(&c)
+	return c.RenderJSON("")
 }
 
 func (c App) Plugins() revel.Result {
-	return c.RenderJSON(app.SendPlugins()) 
+	prepareHeaders(&c)
+	return c.RenderJSON(app.SendPlugins())
 }
 
 func (c App) Workflows() revel.Result {
+	prepareHeaders(&c)
 	return c.RenderJSON(app.SendWorkflows())
 }
 
@@ -27,7 +37,6 @@ func (c App) UpdateWorkflow(channel string) revel.Result {
 	return c.RenderJSON(app.UpdateWorkflow(channel, jsonData))
 }
 
-
 func (c App) RunWorkflow(channel string) revel.Result {
 	var jsonData map[string]interface{}
 	c.Params.BindJSON(&jsonData)
@@ -35,8 +44,7 @@ func (c App) RunWorkflow(channel string) revel.Result {
 	return c.RenderJSON(app.RunWorkflow(channel, jsonData))
 }
 
-
 func (c App) SystemConfig() revel.Result {
+	prepareHeaders(&c)
 	return c.RenderJSON(app.SendSystemInputs())
 }
-
